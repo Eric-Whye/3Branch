e3d7e679a370b8ecbb88a8a21642c3bb50465ca7
highqualitybean <tjobs959@gmail.com>
2022-04-03 03:50:04 +0100

    Bug fixes

--- src/main/java/com/ThreeBranch/Week3/SubMain.java
+++ src/main/java/com/ThreeBranch/Week3/SubMain.java
@@ -1,9 +1,17 @@
 package com.ThreeBranch.Week3;
 
+import com.ThreeBranch.*;
+
 public class SubMain {
   public static void handle() {
-    FileProcessor fp = new FileProcessor();
-    Graph data = fp.getGraph();
-    fp.writeGraphToFile(data);
+    try {
+      Configuration.initialise(Configuration.ConfigFilename);
+      
+      FileProcessor fp = new FileProcessor();
+      Graph data = fp.getGraph();
+      fp.writeGraphToFile(data);
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
   }
 }
