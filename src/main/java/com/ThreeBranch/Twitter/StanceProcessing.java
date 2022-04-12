ba026eca8684cfa8239813c46d11f695477c7248
Eric-whye <EricWhye222@gmail.com>
2022-04-12 14:17:13 +0100

    Improved print coverage

--- src/main/java/com/ThreeBranch/Twitter/StanceProcessing.java
+++ src/main/java/com/ThreeBranch/Twitter/StanceProcessing.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
+
 public class StanceProcessing {
     private final Graph graph;
 
@@ -18,7 +19,7 @@ public class StanceProcessing {
     }
 
     /**
-     *
+     *Assign Stances to the recorded influential users
      */
     public void initialiseStances(){
         BufferedReader reader = null;
@@ -122,6 +123,10 @@ public class StanceProcessing {
 
     public String stanceCoverage(){
         StringBuilder str = new StringBuilder();
+        Double[] list = {
+                0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+                0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,
+                0.0};
 
         int sumPos = 0;
         int sumNeg = 0;
@@ -130,16 +135,68 @@ public class StanceProcessing {
             if (((User)p).getStance().isPresent()) {
                 sumStances++;
 
-                if (((User) p).getStance().get() > 0)
-                    sumPos++;
-                else if (((User) p).getStance().get() < 0)
+                int number = ((User) p).getStance().get();
+                int firstDigit = Integer.parseInt(Integer.toString(Math.abs(number)).substring(0, 1));
+
+                if (number < 0) {
                     sumNeg++;
+                    //For the number -1000
+                    if (number == Integer.parseInt(Configuration.getValueFor("stance.minStance"))){
+                        list[9]++;
+                        continue;
+                    }
+                    //For the numbers that at single or double digits
+                    if (number >= -99){
+                        list[0]++;
+                        continue;
+                    }
+                    list[firstDigit]++;
+                }
+                else{//Numbers Greater than zero
+                    sumPos++;
+                    //For the number 1000
+                    if (number == Integer.parseInt(Configuration.getValueFor("stance.maxStance"))){
+                        list[19]++;
+                        continue;
+                    }
+                    //For the numbers that at single or double digits
+                    if (number <= 99){
+                        list[10]++;
+                        continue;
+                    }
+                    list[firstDigit+10]++;
+                }
             }
         }
 
         str.append("Coverage: " + ((double)sumStances/ graph.size())*100 + "%" + "\n");
         str.append("Positive Stances: " + ((double)sumPos / (double)sumStances)*100 + "%" + "\n");
         str.append("Negative Staneces: "+ ((double)sumNeg / (double)sumStances)*100 + "%" + "\n");
+
+        for(int i = 0; i < list.length;i++){
+            list[i] = (list[i] / (double)sumStances)*100;
+        }
+
+        str.append("-1000 - -900\t" + list[9] + "%\n");
+        str.append("-899 - -800\t" + list[8] + "%\n");
+        str.append("-799 - -700\t" + list[7] + "%\n");
+        str.append("-699 - -600\t" + list[6] + "%\n");
+        str.append("-599 - -500\t" + list[5] + "%\n");
+        str.append("-499 - -400\t" + list[4] + "%\n");
+        str.append("-399 - -300\t" + list[3] + "%\n");
+        str.append("-299 - -200\t" + list[2] + "%\n");
+        str.append("-199 - 100\t" + list[1] + "%\n");
+        str.append("-099 - -001\t" + list[0] + "%\n");
+        str.append("000 - 099\t" + list[10] + "%\n");
+        str.append("100 - 199\t" + list[11] + "%\n");
+        str.append("200 - 299\t" + list[12] + "%\n");
+        str.append("300 - 399\t" + list[13] + "%\n");
+        str.append("400 - 499\t" + list[14] + "%\n");
+        str.append("500 - 599\t" + list[15] + "%\n");
+        str.append("600 - 699\t" + list[16] + "%\n");
+        str.append("700 - 799\t" + list[17] + "%\n");
+        str.append("800 - 899\t" + list[18] + "%\n");
+        str.append("900 - 1000\t" + list[19] + "%\n");
 
         return str.toString();
     }
